204e7bc0881e7ef7d60bfeb69ac96baac7ec7dfb
JRivas491 <jonathanarivas503@gmail.com>
2025-09-17 23:53:13 -0400

    update

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      system.out.println("hello world");
+      
+      String name = "Johnny Bravo";
+      
+      system.out.println(name);
+   } // end main
+} // end class
