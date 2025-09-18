d16d75899d56e8f6be8d74b2d0714e26af939fa7
JRivas491 <jonathanarivas503@gmail.com>
2025-09-17 23:56:31 -0400

    Revert "create class"
    This reverts commit 29a7049e5ff0c63895f12e02f1a30958a3fbf9f6.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,10 @@ public class HelloWorld
 {
    public static void main(String[] args)
    {
-      System.out.println("hello world");
+      system.out.println("hello world");
       
       String name = "Johnny Bravo";
       
-      System.out.println(name);
+      system.out.println(name);
    } // end main
 } // end class
